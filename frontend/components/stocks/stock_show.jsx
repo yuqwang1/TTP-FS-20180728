6faf657ce10161d3b461e5ab2e915c772f2db1d1
Yuqian Wang <iamwangyuqian@yahoo.com>
2018-12-04 10:28:22 -0500

    condense stage 1

--- frontend/components/stocks/stock_show.jsx
+++ frontend/components/stocks/stock_show.jsx
@@ -12,8 +12,6 @@ class StockShowPage extends React.Component{
     this.state  = {
       timescale: "daily"
     }
-    this.resetData = this.resetData.bind(this);
-    this.tooltipRender = this.tooltipRender.bind(this);
   }
 
   componentDidMount(){
@@ -37,79 +35,6 @@ class StockShowPage extends React.Component{
 
   round(number, places){
     return +(Math.round(number + "e+" + places)  + "e-" + places);
-  }
-
-  tooltipRender(e){
-    if (e.payload && e.payload.length > 0){
-      const price = e.payload[0].payload.close;
-      let change = this.round(e.payload[0].payload.change,8).toString();
-      let pctChange = e.payload[0].payload.pctchange.toFixed(2);
-      if (pctChange.toString().includes("Infinity") || pctChange.toString().includes("NaN")){
-        pctChange = "0";
-        change = "0";
-      }
-      const date = new Date(e.payload[0].payload.time*1000);
-      let hour, minutes, time;
-      if (date.getMinutes() < 10){
-        minutes = "0" + date.getMinutes().toString();
-      }else{
-        minutes = date.getMinutes();
-      }
-      if (date.getHours() > 12){
-        hour = date.getHours()%12;
-        if (hour === 0){
-          time = `12:00 AM`
-        }else{
-          time = `${hour}:${minutes} PM`
-        }
-      }else{
-        hour = date.getHours()%12;
-        if (hour === 0){
-          time = `12:00 PM`
-        }else{
-          time = `${hour}:${minutes} AM`
-        }
-      }
-
-      const day = date.toDateString();
-      document.getElementById("pricelabel").innerHTML = "$"+price;
-
-      if (change < 0){
-        document.getElementById("pctChangeLabel").innerHTML = `-$${change.slice(1)} (${pctChange}%)`;
-      }else{
-        document.getElementById("pctChangeLabel").innerHTML = `+$${change} (${pctChange}%)`;
-      }
-      if (this.state.timescale === "daily" || this.state.timescale === "weekly"){
-        return(
-          <div>{time} {day.slice(4,-5)}</div>
-        )
-      }else{
-        return(
-          <div>{day.slice(4,-5)}, {day.slice(-5)}</div>
-        )
-      }
-    }
-  }
-
-  resetData(){
-      let initialPrice;
-      let change;
-      let initialChange;
-      let monthly;
-      let pctChange;
-        const monthData = this.props.data[this.state.timescale] || [];
-        pctChange = monthData[monthData.length-1].pctchange.toFixed(2);
-        initialChange = this.round(monthData[monthData.length-1].change,8);
-        if (initialChange < 0){
-          initialChange = `-$${initialChange.toString().slice(1)} (${pctChange}%)`;
-        }else{
-          initialChange = `+$${initialChange} (${pctChange}%)`;
-        }
-        initialChange = initialChange
-        initialPrice = "$" + monthData[monthData.length-1].close;
-        monthly = this.props.data[this.state.timescale].slice(1);
-        document.getElementById("pricelabel").innerHTML = initialPrice;
-        document.getElementById("pctChangeLabel").innerHTML = initialChange;
   }
 
   clearTimescaleButtons(){
